bcd1a6aa7c5455b18603c4b8a0be670292e3145a
Sanne Jimkes <jimk0001@hz.nl>
2026-01-06 15:37:32 +0100

    Add observer list and notifications to HogwartsGame

--- src/game/HogwartsGame.java
+++ src/game/HogwartsGame.java
@@ -1,8 +1,15 @@
 package game;
 
+import behavioural.GameEventObserver;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HogwartsGame {
 
     private static HogwartsGame instance;
+
+    // List of observers
+    private List<GameEventObserver> observers = new ArrayList<>();
 
     private HogwartsGame() {}
 
@@ -13,11 +20,29 @@ public class HogwartsGame {
         return instance;
     }
 
+    // Start / end game
     public void startGame() {
         System.out.println("🏰 Welcome to Hogwarts Duel!");
     }
 
     public void endGame() {
         System.out.println("✨ Game over!");
+    }
+
+    // Add observer
+    public void addObserver(GameEventObserver observer) {
+        observers.add(observer);
+    }
+
+    // Remove observer
+    public void removeObserver(GameEventObserver observer) {
+        observers.remove(observer);
+    }
+
+    // Notify all observers about an event
+    public void notifyObservers(String event) {
+        for (GameEventObserver observer : observers) {
+            observer.notify(event);
+        }
     }
 }
